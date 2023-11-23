a76a7173ba44abbc7fb65ea4deb4302bde6eaad6
nguyen anh tuan <anhtuan261101@gmail.com>
2023-11-23 20:25:42 +0700

    bai3

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Bai 2
+
 function tinhGiaTri(x, y, z) {
   const giaTri = 3 * x ** 2 - 5 * x * y + z ** 4;
   return giaTri;
@@ -9,3 +11,12 @@ const z = 2;
 
 const ketQua = tinhGiaTri(x, y, z);
 console.log("Giá trị của biểu thức là:", ketQua);
+
+
+//Bai 3
+
+const arr = [5, 1, 4, 2, 7];
+const length = arr.length;
+
+for (let i = 0; i < length; i++) {
+  console.log(arr[i]);}
